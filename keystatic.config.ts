cb80d1e3db6806b727370c74f0dfa0c567ac604f
Kamil M. <116840531+proxima812@users.noreply.github.com>
2026-02-13 15:19:33 +0500

    fix

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -4,25 +4,18 @@ import { posts } from "@/fileds/posts";
 import { tags } from "@/fileds/tags";
 import { config } from "@keystatic/core";
 
-const isLocalMode =
-	process.env.NODE_ENV !== "production" ||
-	process.env.KEYSTATIC_LOCAL === "true";
-const hasGithubEnv =
-	Boolean(process.env.KEYSTATIC_GITHUB_CLIENT_ID) &&
-	Boolean(process.env.KEYSTATIC_GITHUB_CLIENT_SECRET) &&
-	Boolean(process.env.KEYSTATIC_SECRET) &&
-	Boolean(process.env.PUBLIC_KEYSTATIC_GITHUB_APP_SLUG);
+// const isLocalMode = import.meta.env.DEV;
 
 export default config({
 	ui: {
 		brand: { name: "ВДА группа БШН" },
 	},
-	storage: isLocalMode || !hasGithubEnv
-		? { kind: "local" }
-		: {
-				kind: "github",
-				repo: "bshnvda/vda-site-main",
-			},
+	storage:
+		// { kind: "local" },
+		{
+			kind: "github",
+			repo: "bshnvda/vda-site-main",
+		},
 	collections: {
 		posts,
 		pageIndex,
